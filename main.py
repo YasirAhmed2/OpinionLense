f8bde20684afb16ff26d0186f2eb3838cdb8be28
YasirAhmed2 <yasirahmed5077@gmail.com>
2025-08-23 08:34:06 -0700

    starting code for initializing comments scrapping

---
+++ main.py
@@ -0,0 +1,9 @@
+from utils.youtube_api import fetch_comments
+import pandas as pd
+
+if __name__ == "__main__":
+    video_id = "dQw4w9WgXcQ"  # Replace with your test video ID
+    comments = fetch_comments(video_id)
+    df = pd.DataFrame(comments, columns=["Comment"])
+    print(df.head())
+    df.to_csv("data/raw_comments.csv", index=False)
